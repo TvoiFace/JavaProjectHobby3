75cf12bdb230942e8797d816da0219c9fd3698dd
TvoiFace <bogdan.xarchenko@gmail.com>
2019-10-20 14:40:38 +0300

    1

--- src/Hobby.java
+++ src/Hobby.java
@@ -1,53 +1,25 @@
-public class Hobby
+public abstract class Hobby
 {
-    private String hobby;
+    private String name;
     private int age;
-    private char quant;
-    private long love;
-    private float howlong;
-    private double hour;
-    private short times;
-    private boolean key;
-    private byte mark;
 
     public Hobby()
     {
-
     }
 
-    public Hobby(String hobby, int age, char quant, long love, float howlong, double hour, boolean key)
+    public Hobby(String name, int age)
     {
-        this.hobby = hobby;
+        this.name = name;
         this.age = age;
-        this.quant = quant;
-        this.love = love;
-        this.howlong = howlong;
-        this.hour = hour;
-        this.times = times;
-        this.key = key;
-        this.mark = mark;
-    }
-
-    public Hobby(String hobby, int age, char quant, long love, float howlong, double hour, short times, boolean key, byte mark)
-    {
-        this.hobby = hobby;
-        this.age = age;
-        this.quant = quant;
-        this.love = love;
-        this.howlong = howlong;
-        this.hour = hour;
-        this.times = times;
-        this.key = key;
-        this.mark = mark;
     }
 
 
-    public String getHobby() {
-        return hobby;
+    public String getName() {
+        return name;
     }
 
-    public void setHobby(String hobby) {
-        this.hobby = hobby;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
@@ -58,66 +30,5 @@ public class Hobby
         this.age = age;
     }
 
-    public char getQuant() {
-        return quant;
-    }
-
-    public void setQuant(char quant) {
-        this.quant = quant;
-    }
-
-    public long getLove() {
-        return love;
-    }
-
-    public void setLove(long love) {
-        this.love = love;
-    }
-
-    public float getHowlong() {
-        return howlong;
-    }
-
-    public void setHowlong(float howlong) {
-        this.howlong = howlong;
-    }
-
-    public double getHour() {
-        return hour;
-    }
-
-    public void setHour(double hour) {
-        this.hour = hour;
-    }
-
-    public short getTimes() {
-        return times;
-    }
-
-    public void setTimes(short times) {
-        this.times = times;
-    }
-
-    public boolean isKey() {
-        return key;
-    }
-
-    public void setKey(boolean key) {
-        this.key = key;
-    }
-
-    public byte getMark() {
-        return mark;
-    }
-
-    public void setMark(byte mark) {
-        this.mark = mark;
-    }
-
-    protected void tellAboutHobby()
-    {
-        System.out.println("Ur hobby is " + hobby + ".\nU are " + age + ".\nU have  " + quant + " hobby.\n" +
-                 "U love it " + love + "/10.\nU have been doing this for " + howlong + "years.\nU do it" + hour +
-                " hours a day.\n" + times + " a week.\n" + "Our mark is " + key + "/10");
-    }
+    protected abstract void tellAboutHobby();
 }
--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,52 @@ public class Main
 {
     public static void main(String[] args)
     {
+        Hobby[] Arr = new Hobby[4];
+
         System.out.println("Hello, tell me some information about u");
-        Hobby obj1 = new Hobby();
-        Hobby obj2 = new Hobby("Swimming",17, '1', 10, 2, 1, true );
-        Hobby obj3 = new Hobby();
+
         Scanner scn = new Scanner(System.in);
-        System.out.println("What is ur hobby?");
-        String hobby = scn.nextLine();
-        obj1.setHobby(hobby);
+
+        System.out.println("What is ur name");
+        String name = scn.nextLine();
+
         System.out.println("How old are u?");
         int age = scn.nextInt();
-        obj1.setAge(age);
-        char quant = '1';
-        obj1.setQuant(quant);
-        System.out.println("How much do u like it (1...10)?");
-        long love = scn.nextInt();
-        obj1.setLove(love);
-        System.out.println("how long have you been doing this?");
-        float howlong = scn.nextFloat();
-        obj1.setHowlong(howlong);
-        System.out.println("how much time do you spend on a hobby?");
-        double hour = scn.nextDouble();
-        obj1.setHour(hour);
-        System.out.println("how many times a week do you do this?");
-        short times = scn.nextShort();
-        obj1.setTimes(times);
-        System.out.println("Would you like to do this in the future (True/False)?");
-        boolean key = scn.nextBoolean();
-        obj1.setKey(key);
-        System.out.println("Rate the survey (1...10)");
-        byte mark = scn.nextByte();
-        obj1.setMark(mark);
 
-        Hobby Arr[];
-        Arr = new Hobby[3];
+        System.out.println("What is ur favourite dish?");
+        String dish = scn.next();
+
+        System.out.println("How long have u been cooking?");
+        int howlong = scn.nextInt();
+
+
+        Hobby obj1 = new Cooking(name, age, dish, howlong);
+        Hobby obj2 = new Cooking("John", 19, "cream soup", 3);
+
+        System.out.println("What is ur name");
+        String name1 = scn.next();
+
+        System.out.println("How old are u?");
+        int age1 = scn.nextInt();
+
+        System.out.println("The height of the largest wave u seen");
+        int high = scn.nextInt();
+
+        System.out.println("Where are u surfing?");
+        String place = scn.next();
+
+        Hobby obj3 = new Surfing(name1, age1, high, place);
+        Hobby obj4 = new Surfing("Nick", 19, 5, "Gold Coast");
+
         Arr[0] = obj1;
         Arr[1] = obj2;
         Arr[2] = obj3;
-        for(int i = 0; i < 3; i++)
+        Arr[3] = obj4;
+
+        for(Hobby a : Arr)
         {
-            Arr[i].tellAboutHobby();
-            System.out.println("\n");
+            a.tellAboutHobby();
+            System.out.println();
         }
     }
 }
